dff400f852275c8a1c531c6c3a0c8b98cf1a5321
faydaisistema-cmd <faydaisistema@gmail.com>
2026-08-14 00:42:38 -0300

    Delete functions directory

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-exports.importInvites = require('./src/importInvites').importInvites;
-exports.submitResponse = require('./src/submitResponse').submitResponse;
-exports.computeAggregateReport = require('./src/computeAggregateReport').computeAggregateReport;
